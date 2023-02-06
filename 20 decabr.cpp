a10512aed24b45f574c91824fa0842b43f2bd899
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:11:51 +0500

    mohinur

---
+++ 20 decabr.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostrem>
+#include <cstdlib>
+using namespace std;
+void play(int m);
+int main()
+{
+	int opition;
+	int magic;
+	magic = rand();
+	do {
+		cout << "1.\n";
+		cout << "2.\n";
+		cout << "3.\n";
+		do {
+			cout << "n: ";
+			cin >> optio;
+			)while (option < 1 || option>3);
+			switch (option)(
+				case 1:
+					magic = rand();
+					break;
+				case 2:
+					play = (magic);
+					break;
+				case 3:
+					cout << "\n";
+					break;
+		}
+	} while (option != 3);
+	return 0;
+}
+int t, x;
+for (t = 0; t < 100; t++) {
+	cout << "n:";
+	cin >> ;
+	if (x == m) {
+		cout << "" * *\n";
+			return;
+	}
+	else
+		if (x < m) cout << "\n";
+		else cout << "\n";
+}
+cout << "\n.";
+}
