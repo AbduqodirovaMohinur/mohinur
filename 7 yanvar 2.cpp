bd29bdf4fe646be0d8935be2c80d1d8f81a24d60
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:26:22 +0500

    mohinur

---
+++ 7 yanvar 2.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	char str[80];
+	cout << "qator kiriting";
+	cin >> str;
+	cout << "manabu sizning qatoringiz";
+		cout<<str;
+	return 0;
+}
