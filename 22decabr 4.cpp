51bb36ba473bb14abbb30f12470f4cda4d596e48
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:03:24 +0500

    mohinur

---
+++ 22decabr 4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cstdio>
+using namespace std;
+int main()
+{
+	char str[80];
+	char token[80];
+	char* p, * q;
+	cout << "taklif kiriting:";
+	gets(str);
+	p = str;
+	while (*p) {
+		q = token;
+		while (*p != '' && *p) {
+			*q = *p;
+			q++; p++;
+		}
+		if (*p) p++;
+		*q = '\0';
+		cout << token << '\n';
+	}
+	return 0;
+}
