dec3eac3e257b4faff10726e40c662c215a759f1
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:10:42 +0500

    mohinur

---
+++ 21 yanvar 5.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int div_zero();
+int main()
+{
+	int i, j, result;
+	cout << "boluvchi va bolinuvchi:";
+	cin >> i >> j;
+	result = j ? i / j : div_zero();
+	cout << "natija:" << result;
+	return 0;
+}
+int div_zero()
+{
+	cout << "nolga bolish mumkun emas.\n";
+	return 0;
+}
