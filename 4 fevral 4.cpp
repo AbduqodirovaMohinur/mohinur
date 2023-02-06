383a4016cc7963c019672901997c7a40c86cb795
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:30:22 +0500

    mohinur

---
+++ 4 fevral 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+class p_exmple {
+	int num;
+public:
+	void set_num(int val) { num = val; }
+	void show_num();
+};
+void P_exmple::show_num()
+{
+	cout << num << "\n";
+}
+int main()
+{
+	p_exmple ob[2], * p;
+	ob[0].set_num(10);
+	ob[1].set_num(20);
+	p = &ob[0];
+	p->show_num();
+	p++
+		p->show
+}
