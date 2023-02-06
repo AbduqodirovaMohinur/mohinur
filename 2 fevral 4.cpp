1e52dd11e067929444d876809c5ff964cf81852f
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:34:24 +0500

    mohinur

---
+++ 2 fevral 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+struct cl {
+	int get_i();
+	void put_i(int j);
+private:
+	int i;
+};
+int cl::get_i()
+{
+	return i;
+}
+void cl::put_i(int j)
+{
+	i = j;
+}
+int main()
+{
+	cl s;
+	s.put_i(10);
+	cout << s.get_i();
+}
