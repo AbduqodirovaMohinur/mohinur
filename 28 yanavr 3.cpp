769dd124c5e9f26d23a16fab2c137144d2e4d7ae
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 21:54:33 +0500

    mohinur

---
+++ 28 yanavr 3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	union {
+		short int count;
+		char ch[2];
+	};
+	ch[0] = 'x';
+	ch[1] = 'y';
+	cout << "sinvlarning birikmasi:" << ch[0] << ch[1] << '\n';
+	cout << "butun soning birikmasi:" << count << '\n';
+	return 0;
+}
