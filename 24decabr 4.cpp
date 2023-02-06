7d700f20164f1fbe348ab2e0fde8bb39580d2e2b
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 21:56:09 +0500

    mohinur

---
+++ 24decabr 4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <conio.h>
+char* fortunes[] = {
+	"sizga yaqinda pul keladi \n",
+	"sizning hayotingizga sevgi kirib keladi \n",
+	"siz uzoq baxtli hayot kechirasz \n",
+	"hozir tikkan pulingiz qaytib keladi \n",
+	"sizni yaqin dostingiz sizni izlaydi \n".
+};
+int main() {
+	int chance;
+	cout << ":";
+	while (!kbhit()) rand();
+	cout << '\n';
+	chance = rand();
+	chance = chance % 5;
+	cout << fortunes[chance];
+	return 0;
+}
