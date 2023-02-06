4d2ad07690c47ada19fcf6eae512b2fbc7655ee1
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:16:00 +0500

    mohinur

---
+++ 17 yanvar 4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+usigned int j;
+usigned int delay;
+int main()
+{
+	rigester usigned int j;
+	long start end;
+	start = clock()
+		for (delay = 0; delay < 50; delay++)
+			for (i = 0; i < 64000000; i++);
+	end = clock();
+	cout << "tikllash soni:";
+	cout << end - start << '\n';
+	start = clock()
+		for (delay = 0; delay < 50; delay++)
+			for (i = 0; i < 64000000; i++);
+	end = clock();
+	cout << "tikllash soni:";
+	cout << end start << '\n';
+	return 0;
+}
