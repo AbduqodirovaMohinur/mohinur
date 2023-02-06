6a6febf394a573992c261f6aef96240afc32c01d
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:07:23 +0500

    mohinur

---
+++ 21 yanvar 55.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	int* i;
+	double* j;
+	i = (int*)malloc(sizeof(int));
+	if (!i) {
+		cout << "xotirada joy ajratilmadi.\n";
+		return 0;
+	}
+	j = (double*)malloc(sizeof(double));
+	if (!j) {
+		cout << "xotrirada joy ajratilmadi.\n";
+		return 0;
+	}
+	*i = 10;
+	*j = 100.123;
+	cout << *i << ' ' << *j;
+	free(i);
+	free(j);
+	return 0;
+}
