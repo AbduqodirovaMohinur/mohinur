a8bc1595e312636134d1c94843e74fb203703f28
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:21:57 +0500

    mohinur

---
+++ 10 decabr.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int magic;//
+	int tahmin;//
+	magic = rand();//
+	cout << "oylagan sonni kiriting";
+		cin >> tahmin;
+		if (tahmin == magic) cout << "togri";
+		else cout << "...notogri";
+		return 0;
+}
