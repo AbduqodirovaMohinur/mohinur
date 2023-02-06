79668a17e825a4dbd34d8b37d19ab04c4195d4f3
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 21:33:36 +0500

    mohinur
    4

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int sehirli;
+	int taxmin;
+	sehirli = rand();
+	cout << o'ylagan soni kiriting:";
+		cin >> guess;
+	if (guess == magic)
+		cout << "to'g'ri";
+	else
+		cout << "no to'g'ri";
+	return 0;
