47339b5ba9b0fb08cbb311e58467ad63fc15d5e9
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:13:30 +0500

    mohinur

---
+++ 19 yanvar 8.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+enum apple { jonathan, golden_del, winesap, cortland, mcintosh };
+char name[][20] = { 
+	"jonnathan",
+	"golden_delicious",
+	"red_delicious",
+	"winesap",
+	"cortland",
+	"mcintosh",
+};
+int main()
+{
+	apple fruit;
+	fruit = jonathan;
+	cout << name[fruit] << '\n';
+	fruit = winesap;
+	cout << name[fruit] << '\n';
+	fruit = mcintosh;
+	cout << name[fruit] << '\n';
+	return 0;
+}
