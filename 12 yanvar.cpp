11f02c9b0db578904880ae9c4df277a6c6b379ce
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:20:02 +0500

    mohinur

---
+++ 12 yanvar.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+void drill();
+int hisob;
+int num_right;
+int main()
+{
+	cout << "praktichiski mashiqlar nechta:";
+	cin >> hisob;
+	num_right = 0;
+	do {
+		drill(); hisob--;
+	} while (hisob);
+	cout << "siz" << num_right << "togri javob berdingiz.\n";
+	return 0;
+}
+void drill()
+{
+	int count;
+	int a, b, ans;
+	a = rand() % 100;
+	b = rand() % 100;
+	for (hisob = 0; hisob < 3; hisob++) {
+		cout << "nechta boladi" << a << "+" << b << "?";
+		cin >> ans;
+		if (ans == a + b) {
+			cout << "togri\n";
+			num_right++;
+			return;
+		}
+
+	}
+	cout << "sz ozingizni hamma imkoniyatlaringizdan foydalandingiz.\n";
+	cout << "javob teng" << a + b << '\n';
+}
