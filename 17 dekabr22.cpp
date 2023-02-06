6b94b14421d966abd1337e431d67202750551715
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:18:02 +0500

    mohinur

---
+++ 17 dekabr22.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostram>
+using namespake std;
+int main()
+{
+	int choise; 
+	cout << ":\n\n"
+	cout << "1.for\m";
+	cout << "2.if\n";
+	cout << "3.switch\n\n";
+	cout << "mavzuni kiriting:(1-3):;";
+	cin >> choise;{
+	case 1:
+		switch: (choise) {
+		case 1:
+		cout << "for-eng mukammal skill.\n";
+		case 2:
+
+
