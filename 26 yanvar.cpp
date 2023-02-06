528f5bb3a1bdb04756ed23bdf5b6724933fd7a81
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 21:55:07 +0500

    mohinur

---
+++ 26 yanvar.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+struct mystruct {
+	int a; int b;
+};
+mystruct& f(mystruct& var);
+int main()
+{
+	mystruct x, y;
+	x.a = 10; x.b = 20;
+	cout << "hozirdagi elementlar x.a and x.b:";
+	cout << x.a << ' ' << x.b << '\n';
+	y = f(x);
+	cout << "ozgartirilgan element x.a i x.b:";
+	cout << x.a << ' ' << x.b << '\n';
+	cout << "ozgartirilgan element y. a i y.b:";
+	cout << y.a << ' ' << y.b << '\n';
+	return 0;
+}
+mystruct& f(mystruct& var)
+{
+	var.a = var.a * var.a;
+	var.b = var.b / var.b;
+	return var;
+}
