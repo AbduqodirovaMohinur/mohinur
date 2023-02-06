4d46ab425d6710173a06b48cba5ca52c6645bea5
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 21:32:44 +0500

    mohinur
    3

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+class myclass {
+public:
+	myclass() { cout << "oddiy konstruksiya.\n"; }
+	myclass(const myclass obj) {
+		cout << "konstruktsiya nusxasi.\n";
+	}
+};
+myclass f()
+{
+	myclass ob;
+	return ob;
+}
+int main()
+{
+	myclass a;
+	a = f();
+	return 0;
+}
