e132c2c006a67777ba586fb88a2dabe0793eb50b
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:19:28 +0500

    mohinur

---
+++ 13 decabr.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+	int magic;//
+	int guess;//
+	magic = rand();
+	cout << "taxminingizni kiriting";
+	cin >> guess;
+		if(guess==magic) {
+		cout << "**togri**/n";
+		cout << magic << " sehirli son shunga teng /n";
+	}
+	else {
+		cout << "...sehirli son bunga teng emas.";
+		if (guess > magic); cout << " taxmin magic sondan koproq ./n";
+		else cout << "taxmin magic sondan koproq/n";
+	}
+	return 0;
+}
