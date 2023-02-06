55f60da58a52e666a8a72e2240cc95f556597cc2
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:30:43 +0500

    mohinur

---
+++ 4 fevral 3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+class P_example {
+	int num;
+public:
+	void sed_num(int val) { num = val; }
+	void show_num();
+};
+void P_example::show_num()
+{
+	cout << num << "\n";
+}
+int main()
+{
+	P_example ob, * p;
+	ob.sed_num(1);
+	ob.show_num();
+	p = &ob;
+	p->show_num();
+	return 0;
+}
