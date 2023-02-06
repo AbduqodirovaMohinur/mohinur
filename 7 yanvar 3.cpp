0a6887e1213eac4be307aa96e094203775daea68
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:24:49 +0500

    mohinur

---
+++ 7 yanvar 3.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <iostream>
+using namespace std;
+int main()
+{
+	char str[80];
+	cout << "qator kiriting:";
+	gets (str);
+	cout << "manabu siznig qatoringiz:";
+	cout << str;
+	return 0;
+}
