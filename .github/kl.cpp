624e003e2d314831bccac965d9724851aa44f41a
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 21:27:19 +0500

    mohinur
    1

---
+++ .github/kl.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	for (x = 0; x < 7; x++) {
+		if (x == 1) cout << "x birga teng.\n";
+		else if (x == 2) cout << "x ikkiga teng.\n";
+		else if (x == 3) cout << "x uchga teng.\n";
+		else if (x == 4)cout << "x torga teng.\n";
+		else if (x == 5)cout << "x beshga teng"
+		else if (x == 6)cout << "x oltiga teng"
+		else if (x == 7)cout << "x yettiga teng"
+		else cout << "x 1 dan 4 gacha mas.\n";
+		
+	}
+	return 0;
+}
