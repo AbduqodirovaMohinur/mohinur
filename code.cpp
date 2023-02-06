605cc60727d3c439cd0ae2afac0a09de9066e146
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 21:34:54 +0500

    mohinur

---
+++ code.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c, d;
+	cout << "torta son kiriting:";
+		cin >> a >> b;
+		if (b) cout << a / b <<' /n';
+		else cout << "no'lga teng bo'lishi mumkun emas./n";
+		return 0;
+}
