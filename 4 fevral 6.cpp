9b6b509d8dcded2f66dd709cd0e4907cb89afeba
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:30:02 +0500

    mohinur

---
+++ 4 fevral 6.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+class myclass {
+	int* p;
+public:
+	myclass(int i)
+		myclass(const myclass ob);
+	myclass();
+	int getval() { return *p; }
+};
+myclass::myclass::myclass(const myclass ob)
+{
+	p = new int;
+	*p = *ob.p;
+	cout << "p xotira konstruksi ichun yaratilgan.\n";
+}
+myclass::myclass(int i)
+{
+	cout << "oddiy konstruksksiya bilan p xotira ajratilishi";
+	p = new int;
+	*p = i;
+}
+myclass::myclass()
+{
+	cout << "p xotira tozalash.\n";
+	dalete d;
+}
+int main()
+{
+	myclass a(10);
+	myclass b = a;
+	return 0;
+}
