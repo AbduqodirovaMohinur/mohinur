bb5ad24164821a363b73e7c207d025e76c347971
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 21:58:27 +0500

    mohinr

---
+++ 24 decabr.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <iostream>
+using namespace std;
+int main()
+{
+	char str[80];
+	char token[80];
+	int i, j;
+	cout << ":";
+	gets(str);
+	for (i = 0; ; i++) {
+		for (j = 0; str[i] != '' && str[i]; j++, i++)
+			token[j] = str[i];
+		token[j] = '\0';
+		cout << token << '\n';
+		if (!str[i]) break;
+	}
+	return 0;
+}
