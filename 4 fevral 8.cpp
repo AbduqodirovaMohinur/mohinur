f92eb8a484164554213092116e80e2ace055ff2d
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-06 22:29:43 +0500

    mohinur

---
+++ 4 fevral 8.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+class myclass {
+	int* p;
+public:
+	myclass(int i);
+	myclass(const myclass& ob);
+	myclass();
+	int galval() { return *p; }
+};
+myclass::myclass(const myclass& obj)
+{
+	p = new int;
+	*p = obj.p;
+	cout << "konstruksiyani kopiyasi chiqarildi";
+	myclass
+}
