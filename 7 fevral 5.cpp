3782be7a2088b2f764b4278476577de3b9884485
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-07 16:43:37 +0500

    mohinur

---
+++ 7 fevral 5.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	char latter1 = 0;
+	char latter2 = 0;
+	char latter3 = 0;
+	char latter4 = 0;
+	char latter5 = 0;
+	cout << "5ta harifdan iborat kodni kiriting:" << endl;
+	cin >> latter1 >> latter2 >> latter3 >> latter4 >> latter5;
+	cout << "siz kiritingiz";
+	cin >> latter1 >> latter2 >> latter3 >> latter4 >> latter5;
+	cout << endl;
+	return 0;
+}
