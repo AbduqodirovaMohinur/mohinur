2e684d8c6a4b522a3db72b8d38ea195564e769e9
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-07 16:06:33 +0500

    mohinur

---
+++ 7 fevral 3.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int cups = 6;
+	int spoons = cups;
+	int saucers = cups;
+	int amount = cups + spoons + saucers;
+	cout << "hammasi" << amount << "narsalar" << endl;
+	return 0;
+}
