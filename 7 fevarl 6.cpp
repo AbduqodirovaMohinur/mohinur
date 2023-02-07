c99d36fe9a4ee7922fd7b7265d6623f6a4d3e16f
AbduqodirovaMohinur <124484760+AbduqodirovaMohinur@users.noreply.github.com>
2023-02-07 16:44:30 +0500

    mohinur

---
+++ 7 fevarl 6.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	float num1 = 0;
+	float num2 = 0;
+	cout << "birinchi sonni kiriting";
+	cin >> num1;
+	cout << "ikkinchi sonni kiriting";
+	cin >> num2;
+	cout << num1 << "+"<<num2 << "=" << num1 + num2 << endl;
+	cout << num1 << "-" << num2 << "=" << num1 - num2 << endl;
+	cout << num1 << "*" << num2 << "=" << num1 * num2 << endl;
+	cout << "ortacha arfmetika:";
+	cout << num1 << "+" << num2 << "/" << 2 << "=";
+	cout << (num1 + num2) / 2 << endl;
+	cout << endl;
+	return 0;
+}
